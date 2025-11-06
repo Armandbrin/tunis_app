0564f15c73c86b91c1d16664e84a78e8ecab1307
Armand BRIN <armandabrinb@gmail.com>
2025-11-06 17:00:50 +0100

    avancement sur la page d'accueil

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Main />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+function Header() {
+  return (
+    <header className="w-full flex justify-center p-5 border-b border-[#ECEBF5] shadow-[0_2px_10px_rgba(0,0,0,0.5)]">
+      <section className="flex justify-center items-center w-full gap-30">
+        <button className="hover:bg-[#F2F2F2] text-[#E70013] rounded-lg font-bold px-3 py-2 hover:shadow-[5px_5px_5px_rgba(0,0,0,0.5)] transition duration-300 ease-in-out">
+          Menu
+        </button>
+        <button className="hover:bg-[#F2F2F2] text-[#E70013] rounded-lg font-bold px-3 py-2 hover:shadow-[5px_5px_5px_rgba(0,0,0,0.5)] transition duration-300 ease-in-out">
+          Cursus
+        </button>
+        <button className="hover:bg-[#F2F2F2] text-[#E70013] rounded-lg font-bold px-3 py-2 hover:shadow-[5px_5px_5px_rgba(0,0,0,0.5)] transition duration-300 ease-in-out">
+          Cours
+        </button>
+      </section>
+      <h1 className="text-5xl text-center font-bold w-full">Tunis Formation</h1>
+      <section className="flex justify-center items-center w-full gap-30">
+        <button className="hover:bg-[#F2F2F2] text-[#E70013] font-bold rounded-lg px-3 py-2 hover:shadow-[5px_5px_5px_rgba(0,0,0,0.5)] transition duration-300 ease-in-out">
+          Se connecter
+        </button>
+        <button className="hover:bg-[#F2F2F2] text-[#E70013] font-bold rounded-lg px-3 py-2 hover:shadow-[5px_5px_5px_rgba(0,0,0,0.5)] transition duration-300 ease-in-out">
+          S'inscrire
+        </button>
+      </section>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <a>Proposition de cursus</a>
+      <section></section>
+      <a>Proposition de cours</a>
+      <section></section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <section></section>
+    </footer>
+  );
+}
+
+export default App;
